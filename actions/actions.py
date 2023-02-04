4b83f17fbca551e44df73c3797da70c68a5128ec
wiktor bozek <wiktorsebbozek@gmail.com>
2023-02-04 20:23:56 +0100

    validation of booking form

--- actions/actions.py
+++ actions/actions.py
@@ -1,33 +1,89 @@
 import json
 from pathlib import Path
 from typing import Any, Text, Dict, List
-from actions.main import Binance
+from actions.main import RestaurantModel
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
 from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
+from rasa_sdk.types import DomainDict
+import re
+import datetime
+
+def isValidTime(time):
+    try:
+      if int(time) <= 24 and int(time) >= 0: return True
+      return False
+    except:
+      regex = "^([01]?[0-9]|2[0-3]):[0-5][0-9]$"
+      p = re.compile(regex)
+      if (time == ""): return False
+      m = re.search(p, time)
+      if m is None: return False
+      return True
+
+def ValidateDate(date):
+  if date == "today": return True
+  if date == "tomorrow": return True
+  try:
+      datetime.date.fromisoformat(date)
+      return True
+  except ValueError:
+      return False
 
 
+class ValidateBookingForm(FormValidationAction):
+  def name(self) -> Text:
+    return "validate_booking_form"
 
-class ActionGetPrice(Action):
+  def validate_hour(
+    self, 
+    slot_value: Any,
+    dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: DomainDict,
+  ) -> Dict[Text, Any]:
+    """ Validate 'hour' value """
+    if isValidTime(slot_value) == False or isValidTime(slot_value) is None:
+      dispatcher.utter_message(text="Wrong time format. Please use 24-hour format. ex: 17:00, 17")
+      return {"hour": None}
+    
+    dispatcher.utter_message(text=f"All right, you want to book table at {slot_value}.")
+    return {"hour": slot_value}
 
-    def name(self) -> Text:
-      return "action_get_price"
+  def validate_guests(
+    self, 
+    slot_value: Any,
+    dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: DomainDict,
+  ) -> Dict[Text, Any]:
+    """ Validate 'guests' value """
+    try:
+      int(slot_value)
+      dispatcher.utter_message(text=f"All right, you want to book table for {slot_value} guests.")
+      return {"guests": slot_value}
+    except:
+      dispatcher.utter_message(text=f"Please enter the correct number of guests")
+      return {"guests": None}
 
-    def run(self,
-           dispatcher: CollectingDispatcher,
-           tracker: Tracker,
-           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        for entity in tracker.latest_message['entities']:
-            print(tracker.latest_message)
-            if entity['entity'] == 'symbol':
-                symbol = entity['value']
-                
-            else:
-                    dispatcher.utter_message(
-                        text=f"I do not recognize {symbol}, are you sure it is correctly spelled?")
-        return []
+  def validate_date(
+    self, 
+    slot_value: Any,
+    dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: DomainDict,
+  ) -> Dict[Text, Any]:
+    """ Validate 'date' value """
+    if ValidateDate(slot_value) == False or ValidateDate(slot_value) is None:
+      dispatcher.utter_message(text="Wrong date format. Please use YYYY-MM-DD format")
+      return {"date": None}
+    
+    dispatcher.utter_message(text=f"All right, you want to book table on {slot_value}.")
+    return {"date": slot_value}
+
+
 
 
 class ActionBookTable(Action):
